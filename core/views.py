60d8b43aedb65cfc83b8add692d19799915892cd
GPerusato <gustavoperusato@gmail.com>
2022-05-31 01:52:47 -0300

    Incluindo template para inserção de eventos na agenda. Incluido gitignore para ignorar arquivos compilados

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import Event
 from django.shortcuts import redirect, render
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
@@ -32,3 +33,17 @@ def lista_eventos (request):
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        usuario = request.user
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        Evento.objects.create(usuario=usuario, titulo=titulo, data_evento=data_evento, descricao=descricao)
+    return redirect("/")
